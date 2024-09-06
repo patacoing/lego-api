a93e1f9e445fc2279c9ddd80e61c4cef41ba49f9
patacoing <romain.lefebvre1104@gmail.com>
2024-09-06 23:39:13 +0200

    feat: ci
    Added code-validation by using
    mypy and commit message validation

--- api/urls.py
+++ api/urls.py
@@ -1,2 +1,6 @@
-urlpatterns = [
+from functools import partial
+from typing import Union
+from django.urls.resolvers import URLPattern, URLResolver
+
+urlpatterns: list[partial[Union[URLResolver, URLPattern]]] = [
 ]
