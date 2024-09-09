afd848c8a0c92bd4e5864065427f0f9d41726a80
patacoing <romain.lefebvre1104@gmail.com>
2024-09-09 22:10:14 +0200

    refactor: type hinting

--- theme/serializers.py
+++ theme/serializers.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from rest_framework import serializers
 
 from theme.models import Theme
@@ -13,7 +15,7 @@ class CreateThemeSerializer(serializers.Serializer):
     name = serializers.CharField()
     parent_id = serializers.IntegerField(required=False, allow_null=True, default=None)
 
-    def create(self, validated_data) -> Theme:
+    def create(self, validated_data: dict[str, Union[str, int]]) -> Theme:
         return Theme(**validated_data)
 
 
@@ -21,7 +23,7 @@ class UpdateThemeSerializer(serializers.Serializer):
     name = serializers.CharField(required=False)
     parent_id = serializers.IntegerField(required=False, allow_null=True, default=None)
 
-    def update(self, instance, validated_data) -> Theme:
+    def update(self, instance: Theme, validated_data: dict[str, Union[str, int]]) -> Theme:
         instance.name = validated_data.get('name', instance.name)
         instance.parent_id = validated_data.get('parent_id', instance.parent_id)
 
