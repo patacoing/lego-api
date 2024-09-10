04c44b81d9f410da96763f0ed37a71943c3a85c7
patacoing <romain.lefebvre1104@gmail.com>
2024-09-10 23:53:18 +0200

    feat: changed SECRET_KEY default value
    Mypy rose an error during CI but not in dev env

--- lego/settings.py
+++ lego/settings.py
@@ -7,7 +7,7 @@ environ.Env.read_env()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = env('SECRET_KEY', default="")
+SECRET_KEY = env('SECRET_KEY', default="test")
 
 DEBUG = env('DEBUG', default=False)
 
